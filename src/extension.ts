d01bae361e18f660c1a4f6659e19f2774321bbbf
Reidond <reidond@gmail.com>
2019-07-25 13:59:36 +0300

    Add jsonValidation for hemtt project file

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,5 @@
 import * as vscode from "vscode";
 
-export function activate(context: vscode.ExtensionContext) {
-    const disposable = vscode.commands.registerCommand(
-        "extension.helloWorld",
-        () => {
-            vscode.window.showInformationMessage("Hello World!");
-        }
-    );
-    context.subscriptions.push(disposable);
-}
+export function activate(context: vscode.ExtensionContext) {}
 
 export function deactivate() {}
