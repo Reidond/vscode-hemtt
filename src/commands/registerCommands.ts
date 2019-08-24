245813f097dacf395365dd953a33943ab2efc810
Reidond <reidond@gmail.com>
2019-08-25 02:46:14 +0300

    Fix errors

--- src/commands/registerCommands.ts
+++ src/commands/registerCommands.ts
@@ -1,12 +1,16 @@
 import { commands, ExtensionContext } from "vscode";
 import { create } from "./project/create";
 import { init } from "./project/init";
+import { createAddon } from "./project/createAddon";
 
 export function registerCommands(context: ExtensionContext): void {
-  context.subscriptions.push(
-    commands.registerCommand("hemtt.project.create", create)
-  );
-  context.subscriptions.push(
-    commands.registerCommand("hemtt.project.init", init)
-  );
+    context.subscriptions.push(
+        commands.registerCommand("hemtt.project.create", create)
+    );
+    context.subscriptions.push(
+        commands.registerCommand("hemtt.project.init", init)
+    );
+    context.subscriptions.push(
+        commands.registerCommand("hemtt.project.createAddon", createAddon)
+    );
 }
