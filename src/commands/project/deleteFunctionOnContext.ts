ed9dd7330868d347dc8ca5320acc1b95a89a263f
Reidond <reidond@gmail.com>
2019-10-01 09:58:32 +0300

    Add delete function to context menu

--- src/commands/project/deleteFunctionOnContext.ts
+++ src/commands/project/deleteFunctionOnContext.ts
@@ -1,1 +1,10 @@
-export async function deleteFunctionOnContext() {}
+import { Uri } from "vscode";
+import { deleteFunction } from "@shared/deleteFunction";
+
+export async function deleteFunctionOnContext(dir: Uri) {
+  const dirArr = dir.path.split("/");
+  const func = dirArr.pop();
+  const [addon] = dirArr.slice(-2);
+
+  await deleteFunction(addon, func!);
+}
